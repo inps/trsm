7ed8162f50cd5ff2ea26964d76354e584855373d
zhoudl <zhoudl@10.0.130.74>
2016-12-01 18:47:56 +0800

    initial trsm

---
+++ trsm/src/main/java/cn/inps/trsm/Serializer.java
@@ -0,0 +1,14 @@
+package cn.inps.trsm;
+
+import java.io.IOException;
+
+public interface Serializer {
+	void setClassLoader(ClassLoader loader);
+
+	byte[] attributesHashFrom(RedisSession session) throws IOException;
+
+	byte[] serializeFrom(RedisSession session, SessionSerializationMetadata metadata) throws IOException;
+
+	void deserializeInto(byte[] data, RedisSession session, SessionSerializationMetadata metadata)
+			throws IOException, ClassNotFoundException;
+}
